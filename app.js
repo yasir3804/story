c3e0210a946594b01e9d4b762f3db0953982b9bf
yasirmanzoor <yasirmanzoor32@gmail.com>
2022-11-01 14:26:50 +0530

    first updates

---
+++ app.js
@@ -0,0 +1,58 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const https=require("https");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname +'/public'));
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/signup.html")
+})
+app.post("/",(req,res)=>{
+   const firstName=req.body.fName;
+   const lastName=req.body.lName;
+   const email=req.body.email;
+   const data={
+     members:[
+       {
+         email_address:email,
+         status:"subscribed",
+         merge_fields:{
+           FNAME:firstName,
+           LNAME:lastName
+         }
+       }
+     ]
+   }
+
+   const jsonData= JSON.stringify(data)
+const url= "https://us21.api.mailchimp.com/3.0/lists/df39df2078"
+   const options={
+     method:"POST",
+     auth:"yasir:a54965f80dc304586a35a7be5f56961f-us21"
+   }
+const request= https.request(url,options, function(response){
+
+
+
+  if(response.statusCode===200){
+    res.sendFile(__dirname+"/success.html")
+  }else{
+    res.sendFile(__dirname+"/failure.html")
+  }
+  response.on(data,function(data){
+    console.log(data)
+  });
+
+});
+request.write(jsonData);
+request.end();
+});
+
+app.listen(3000,()=>{
+  console.log("server is started and listening on port 3000");
+})
+// KEY
+// a54965f80dc304586a35a7be5f56961f-us21
+
+// AUDIENCE ID
+// df39df2078.
